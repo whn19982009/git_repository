cdf9dff8598df100f3f54251b3ab6ee1a0407e58
wanghaonan <wanghaonan@wintec.cn>
2021-08-18 10:53:00 +0800

    S:test.shell feat:修改代码 Kernel:null

--- test.sh
+++ test.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
+var1=10
+var2=20
+if [[$var1 =~ ^[0-9]]+$ && $var2 =~ ^[0-9]+$ ]];then
+    if [$var1 -lt $var2];then
+        echo "var2>var1"
+    elif [$var1 -eq $var2];then
+        echo "var2==var1"
+    else
+        echo "var1>var2"
+    fi
 
+else
+    echo "please enter the correct number"
+fi
+unset var1 var2
